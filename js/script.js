d8f7cfe50fcf2cf76a99e3ea3148b91219005b24
HamzaJirah <hamzajirah@gmail.com>
2019-06-18 12:33:19 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Check support for service workers
+if(navigator.serviceWorker){
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw_cached_pages.js')
+      .then(reg => console.log('Service Worker: Registered'))
+      .catch(err => console.log(`Service Worker: Error: ${err}`))
+  })
+}
